2039c203e77f2cf4f069a57e8c15b91d5f7308ea
Aleksandr Shvidkiy <alexshved55@gmail.com>
2025-04-13 22:22:33 +0300

    Update Main.java

--- src/main/java/com/gmail/alexshved55/Main.java
+++ src/main/java/com/gmail/alexshved55/Main.java
@@ -2,6 +2,7 @@ package com.gmail.alexshved55;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("How are you?");
+        System.out.println("Hodor");
     }
 }
