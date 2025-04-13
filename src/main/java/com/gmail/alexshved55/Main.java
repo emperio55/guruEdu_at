f0a8045ec056afe0a38baf8c5be00b135c041080
Aleksandr Shvidkii <alexshved55@gmail.com>
2025-04-13 22:17:30 +0300

    Message was change

--- src/main/java/com/gmail/alexshved55/Main.java
+++ src/main/java/com/gmail/alexshved55/Main.java
@@ -2,6 +2,6 @@ package com.gmail.alexshved55;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("How do you do, fellow kids?");
     }
 }
